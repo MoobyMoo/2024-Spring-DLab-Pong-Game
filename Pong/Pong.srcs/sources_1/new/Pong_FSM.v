8edeedf98aa6891225c665ff0eed8311fc34c3d9
Moo <104883858+MoobyMoo@users.noreply.github.com>
2024-05-09 21:16:26 +0800

    Feature ascending ball speed, modes, and fixed ssd (#5)
    * added ascending ball speed
    
    * added modes
    
    * fix mode
    
    * working fast ball and mode
    
    * fixed ssd only counto to 9

--- Pong/Pong.srcs/sources_1/new/Pong_FSM.v
+++ Pong/Pong.srcs/sources_1/new/Pong_FSM.v
@@ -15,6 +15,7 @@ module Pong_FSM #(
     input p1_down,
     input p2_up,
     input p2_down,
+    input change_mode,
 
     output reg out_Hsync = 0,
     output reg out_Vsync = 0,
@@ -22,9 +23,7 @@ module Pong_FSM #(
     output reg [3:0] p2_score = 0,
     output [3:0] out_Red,
     output [3:0] out_Green,
-    output [3:0] out_Blue,
-
-    output reg [2:0] state = 0 
+    output [3:0] out_Blue
     );
 
 
@@ -33,17 +32,20 @@ module Pong_FSM #(
     // now each board unit represents 16*16 pixels
     // This way we only need to keep track of less column and row positions
     parameter GAME_WIDTH = 40, GAME_HEIGHT = 30;
-    parameter SCORE_LIMIT = 9;
     parameter PADDLE_HEIGHT = 6;
     parameter P1_PADDLE_X = 0, P2_PADDLE_X = GAME_WIDTH-1;
-    parameter INIT = 3'd0, IDLE = 3'd1, RUNNING = 3'd2, P1_SCORE = 3'd3, P2_SCORE = 3'd4, OVER = 3'd5;
-
+    parameter INIT = 3'd0, MODE = 3'd1, IDLE = 3'd2, RUNNING = 3'd3, 
+    P1_SCORE = 3'd4, P2_SCORE = 3'd5, OVER = 3'd6;
+    parameter P5 = 2'd0, P10 = 2'd1, P15 = 2'd2;
 
     wire temp_Hsync, temp_Vsync, p1_draw_paddle, p2_draw_paddle, draw, running;
     wire [9:0] column_count, row_count;
     wire [5:0] p1_paddle_y, p2_paddle_y, ball_x, ball_y;
     wire [5:0] small_column_count, small_row_count;
-    reg p1_score_point = 0, pressed = 0;
+    reg p1_score_point = 0, start_pressed = 0, change_mode_pressed = 0;
+    reg [1:0] mode = 0;
+    reg [3:0] score_limit = 5;
+    reg [2:0] state = 0 ;
 
     // Divide by 16
     assign small_column_count = column_count[9:4];
@@ -69,14 +71,24 @@ module Pong_FSM #(
         out_Hsync <= temp_Hsync;
         out_Vsync <= temp_Vsync;
 
-        if (start & ~pressed) begin
-            pressed <= 1'b1;
+        if (start & ~start_pressed) begin
+            start_pressed <= 1'b1;
         end
-        else if (~start & pressed) begin
-            pressed <= 1'b0;
+        else if (~start & start_pressed) begin
+            start_pressed <= 1'b0;
         end
         else begin
-            pressed <= pressed;
+            start_pressed <= start_pressed;
+        end
+
+        if (change_mode & ~change_mode_pressed) begin
+            change_mode_pressed <= 1'b1;
+        end
+        else if (~change_mode & change_mode_pressed) begin
+            change_mode_pressed <= 1'b0;
+        end
+        else begin
+            change_mode_pressed <= change_mode_pressed;
         end
     end
 
@@ -118,11 +130,18 @@ module Pong_FSM #(
     Draw #(
         .P1_PADDLE_X(P1_PADDLE_X),
         .P2_PADDLE_X(P2_PADDLE_X),
-        .PADDLE_HEIGHT(PADDLE_HEIGHT)
+        .PADDLE_HEIGHT(PADDLE_HEIGHT),
+        .INIT(INIT),
+        .MODE(MODE), 
+        .IDLE(IDLE), 
+        .RUNNING(RUNNING), 
+        .P1_SCORE(P1_SCORE), 
+        .P2_SCORE(P2_SCORE),
+        .OVER(OVER)
         ) draw_wrap (
         .clock(clock),
         .p1_paddle_y(p1_paddle_y),
-         .p2_paddle_y(p2_paddle_y),
+        .p2_paddle_y(p2_paddle_y),
         .ball_x(ball_x),
         .ball_y(ball_y),
         .column_count(small_column_count),
@@ -141,8 +160,31 @@ module Pong_FSM #(
         case (state)
         // Start Screen
         INIT: begin
-            state <= (start & ~pressed) ? IDLE : INIT;
+            state <= (start & ~start_pressed) ? MODE : INIT;
+            mode = P5;
+            score_limit <= 5;
         end
+        // Choose how many points to win
+        MODE: begin
+            if (change_mode & ~change_mode_pressed) begin
+                case (mode)
+                P5: begin
+                    mode <= P10;
+                    score_limit <= 10;
+                end
+                P10: begin
+                    mode <= P15;
+                    score_limit <= 15;
+                end
+                P15: begin
+                    mode <= P5;
+                    score_limit <= 5;
+                end
+                endcase
+            end
+
+            state <= (start & ~start_pressed) ? IDLE : MODE;
+        end    
         // Stay in this state until start button is hit
         IDLE: begin
             p1_score <= 0;
@@ -164,7 +206,8 @@ module Pong_FSM #(
         
         P1_SCORE: begin
             p1_score_point <= 1;
-            if (p1_score == SCORE_LIMIT-1) begin
+            if (p1_score == score_limit-1) begin
+                p1_score <= p1_score + 1;
                 state <= OVER;
             end else begin
                 p1_score <= p1_score + 1;
@@ -174,7 +217,8 @@ module Pong_FSM #(
 
         P2_SCORE: begin
             p1_score_point <= 0;
-            if (p2_score == SCORE_LIMIT-1) begin
+            if (p2_score == score_limit-1) begin
+                p2_score <= p2_score + 1;
                 state <= OVER;
             end else begin
                 p2_score <= p2_score + 1;
@@ -183,7 +227,7 @@ module Pong_FSM #(
         end
         //End Screen
         OVER: begin
-            state <= (start & ~pressed) ? INIT : OVER;
+            state <= (start & ~start_pressed) ? INIT : OVER;
         end
         endcase
     end
